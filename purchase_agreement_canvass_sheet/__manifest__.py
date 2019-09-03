e7649dc4c75679ea91f65e8bbd12a6433bc8ce75
artreadyan-agilis <dyan@biz1global.com>
2019-09-03 14:59:55 +0800

    added canvass sheet report for bid price comparison

---
+++ purchase_agreement_canvass_sheet/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Purchase Agreement Bid Comparison Analysis Report",
+    'summary': 'Canvass Sheet for bid comparison',
+    'category': 'Purchase',
+    'author': 'Dyan Estacio',
+    'sequence': 1,
+    'version': '1.0',
+    'depends': [
+        'base','purchase_requisition'
+        ],
+    'data': [
+        'report/bid_comparison_pr_report.xml'
+        
+    ],
+	'installable': True,
+    'application': False,
+
+}
+
+
+
