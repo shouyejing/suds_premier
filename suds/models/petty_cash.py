a2e5787e63f7bad187eeabc251862029267f961e
jardosa <jardosa@live.com>
2020-01-04 16:04:07 +0800

    added additional check when clicking pay button

--- suds/models/petty_cash.py
+++ suds/models/petty_cash.py
@@ -3,6 +3,10 @@
 from odoo import models, fields, api, _
 from datetime import datetime as date
 from odoo.exceptions import ValidationError, UserError, Warning
+import logging
+import itertools
+import calendar
+_logger = logging.getLogger("_name_")
 
 
 class PettyCash(models.Model):
@@ -33,3 +37,26 @@ class PettyCash(models.Model):
             'requested_by_date': date.now(),
         })
         return True
+
+
+class PettyCashLine(models.Model):
+    _inherit = 'petty.cash.line'
+
+    @api.multi
+    def do_pay(self):
+        res = super(PettyCashLine, self).do_pay()
+        for line in self:
+            cash = line.amount if line.state == 'draft' else 0
+            petty_balance = line.cash_id.petty_cash_balance + cash
+            round(petty_balance)
+            _logger.info('\n\n\ntotal cash: {}\npetty cash balance: {}\n\n\n'.format(
+                str(cash), str(petty_balance)))
+
+            if line.cash_id.paid_amount_total > line.cash_id.amount_received:
+                raise UserError(
+                    _('You Can Not Pay More Then  Received Amount !'))
+            elif cash > petty_balance:
+                raise UserError(
+                    _('You Can Not Pay More Than The Petty Cash Balance !'))
+            else:
+                return res
