c15204c0a13fbc8bd421e3e0ad89dc85efa95db1
jardosa <jardosa@live.com>
2020-01-13 17:52:54 +0800

    added requested by and created by fields

--- suds/models/purchase.py
+++ suds/models/purchase.py
@@ -8,6 +8,7 @@ _logger = logging.getLogger(__name__)
 class PurchaseAnalytic(models.Model):
     _inherit='purchase.order'
 
+    requested_by_id = fields.Many2one('res.users', string="Requested By", compute="_get_requestor_name") 
     account_analytic_id = fields.Many2one('account.analytic.account', string="Analytic Account")
     branch_id = fields.Many2one('branches.cost.center', string="Branch", related='account_analytic_id.branch_id')
     company_name = fields.Char(related="company_id.name", string="Company")
@@ -16,6 +17,12 @@ class PurchaseAnalytic(models.Model):
     def submit_po(self):
         return self.write({'state': 'to approve'})
 
+    @api.depends('partner_id')
+    def _get_requestor_name(self):
+        for rec in self:        
+            for i in rec.purchase_request_id:
+                requestor = i.requested_by
+                rec.requested_by_id = requestor
 
 class PurchaseAnalyticOrderLine(models.Model):
     _inherit = 'purchase.order.line'
