8bc5ed46cc8d3b14aed4f7691da0adaf60a4e617
artreadyan-agilis <dyan@biz1global.com>
2020-04-22 17:27:02 +0800

    Add Excel report; Sales and Invoice, Receivable and Payable Report

--- suds_excel_reports/__manifest__.py
+++ suds_excel_reports/__manifest__.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "SUDS Sales and Purchase History Reports",
+    'name': "SUDS Excel Reports",
 
     'summary': """
-        Prints Excel Files of SUDS' sales and purchase history reports""",
+        Prints Excel Files of SUDS' sales and purchase history reports,
+        Print Sales and Invoice Report,
+        Receivable Report,
+        Payable Report""",
 
     'description': """
         Prints Excel Files of SUDS' sales and purchase history reports. Users
         can print 1 document or a selection of documents in the Sales and Purchase Tree View
     """,
 
-    'author': "John Christian Ardosa",
+    'author': "John Christian Ardosa, Ralf Cabarogias - Agilis Group",
     'website': "http://agilis.com.ph",
 
     'category': 'Reports',
@@ -23,6 +26,9 @@
     'data': [
         # 'security/ir.model.access.csv',
         'reports/report.xml',
+        'reports/sales_invoice_report.xml',
+        'reports/receivable_report.xml',
+        'reports/payable_report.xml'
         # 'reports/excel_report_wizard.xml',
     ]
 }
--- suds_excel_reports/reports/__init__.py
+++ suds_excel_reports/reports/__init__.py
@@ -1,2 +1,5 @@
 from . import sales_history
 from . import purchase_history
+from . import sales_invoice_report_wizard
+from . import receivable_report_wizard
+from . import payable_report_wizard
